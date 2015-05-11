616c856d6e68ad96bb419634ca4f3ccb55a08a2c
Alex O'Neill <oneill.alexander.b@gmail.com>
2015-05-10 23:47:48 -0400

    Partial style change

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+// aoneill - 05/09/15
+
+module.exports = {
+  'port':        process.env.PORT || 80,
+  'src':         '/config',
+  'views':       '/views',
+  'subdomains':  '/subdomains',
+  'sass':        '/assets/sass',
+  'static':      '/static',
+  'css':         '/css'
+};
--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 // server.js
 // aoneill - 04/15/15
 
-var port = (process.env.PORT || 80)
-
 // Modules
-var connect = require('connect')
-  , express = require('express')
-  , bodyParser = require('body-parser')
-  , hb = require('express-handlebars')
-  , path = require('path')
-  , url = require('url');
+var config = require('./config.js'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    hb = require('express-handlebars'),
+    sass = require('node-sass-middleware'),
+    path = require('path'),
+    url = require('url');
 
-var _config = path.join(__dirname, 'config');
-var _subdomains = path.join(__dirname, 'subdomains');
+// Paths
+var _config = path.join(__dirname, config.src);
+var _subdomains = path.join(__dirname, config.subdomains);
+var _static = path.join(__dirname, config.static);
+var _sass = path.join(__dirname, config.sass);
 
 // Init
 var server = express();
-server.engine('handlebars', hb());
+server.engine('handlebars', hb({
+  extname: '.hbs'
+}));
 
+// Setup middleware and properties
 server.set('view engine', 'handlebars');
-server.set('views', path.join(__dirname, '/views'));
-server.use(connect.static(__dirname + '/static'));
-server.use(bodyParser.urlencoded({ extended: false }));
+server.set('views', path.join(__dirname, config.views));
+server.use(sass({
+  src: _sass,
+  dest: path.join(_static, config.css),
+  prefix: config.css,
+}));
+server.use(express.static(_static));
+server.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(port);
+// Start up
+server.listen(config.port);
 
 // Sub-configurations
+console.log("Loading subdomains...");
 require(path.join(_config, 'subdomains.js')).load(_subdomains, express, server);
+
+console.log("Loading routes...");
 require(path.join(_config, 'routes.js')).load(server);
 
-console.log('Listening on http://0.0.0.0:' + port);
+console.log('Starting on http://0.0.0.0:' + config.port);
