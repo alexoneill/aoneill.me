343555271627dc558b39d8532d96bb921441b52d
Alex O'Neill <oneill.alexander.b@gmail.com>
2015-10-07 10:47:00 +0000

    Fixed paths to SSL files

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 // config.js
 // aoneill - 05/09/15
 
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
 
 module.exports = {
   'httpPort':    process.env.HTTPPORT || 80,
   'httpsPort':   process.env.HTTPSPORT || 443,
   'credentials': {
-    key:  fs.readFileSync('assets/ssl/ssl.key'),
-    cert: fs.readFileSync('assets/ssl/ssl.crt')
+    key:  fs.readFileSync(path.join(__dirname, 'assets/ssl/ssl.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'assets/ssl/ssl.crt'))
   },
   'src':         '/config',
   'views':       '/views',
